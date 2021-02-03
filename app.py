35def43cfd9fd118480f739a18ac8b2ab12545b5
Nagarjun <nagarjunsa09@gmail.com>
2021-02-04 00:27:27 +0530

    Removed notebooks

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template, request
-import pickle
-import sklearn
-import numpy as np
 import requests
-import jsonify
-from sklearn.preprocessing import StandardScaler
 import nltk
 
 app = Flask(__name__)
@@ -57,50 +52,6 @@ def normalization():
 def noise_removal():
     return render_template('/demos/preprocessing/noise-removal.html')
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    diesel = 0
-    petrol = 0
-    seller_type = 0
-    transmission = 0
-
-    if request.method == 'POST':
-
-        curr_price = float(request.form['Present_Price'])
-        kms_driven = int(request.form['Kms_Driven'])
-        owner = int(request.form['Owner'])
-
-        petrol_val = request.form['Fuel']
-        if(petrol_val == 'Petrol'):
-            petrol = 1
-        else:
-            diesel = 1
-
-        year = int(request.form['Year'])
-        age = 2020 - year
-
-        seller_type_val = request.form['Seller_Type']
-        if(seller_type_val == 'Individual'):
-            seller_type = 1
-        else:
-            seller_type = 0
-
-        transmission_val = request.form['Transmission']
-        if(transmission_val == 'Manual'):
-            transmission = 1
-        else:
-            transmission = 0
-
-        prediction = model.predict([[curr_price, kms_driven, owner, age, diesel, petrol, seller_type, transmission]])
-        output = round(prediction[0],2)
-
-        if(output < 0):
-            return render_template('index.html')
-        else:
-            return render_template('index.html')
-
-    else:
-        return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
